994ca1d75e0f66cc42db4f769cf9b29ab7a127df
Santiago Fernandez Madero <safern@microsoft.com>
2017-12-01 17:04:55 -0800

    Initial commit

---
+++ fullframeworklibrary/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.DirectoryServices.AccountManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fullframeworklibrary
+{
+    public class Class1
+    {
+        public static List<string> GetGroupNames(string userName)
+        {
+            var result = new List<string>();
+
+            if (string.IsNullOrEmpty(userName))
+                return result;
+
+            var domain = userName.Split('\\').FirstOrDefault();
+
+            try
+            {
+                var pc = new PrincipalContext(ContextType.Domain, domain);
+                var identity = UserPrincipal.FindByIdentity(pc, userName);
+                if (identity != null)
+                {
+                    var src = identity.GetGroups(pc);
+                    src.ToList().ForEach(sr => result.Add(sr.SamAccountName));
+                }
+                return result;
+            }
+            catch (PrincipalServerDownException e)
+            {
+                Console.WriteLine($"There was an exception because the server is down: ********** \n\n {e.ToString()}");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ netcoreapp/Program.cs
@@ -0,0 +1,18 @@
+﻿using fullframeworklibrary;
+using System;
+
+namespace netcoreapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = Class1.GetGroupNames("user");
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group);
+            }
+        }
+    }
+}
